d2af0ee9c8d4a82a009aebd5d3b7cb4c05ee5842
Peter Sevcik <psevcikbn@gmail.com>
2019-01-18 15:17:35 +0100

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Peter on 18/01/2019.
+//  Copyright © 2019 Excellence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
